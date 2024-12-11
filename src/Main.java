6ed72830956d90f15cbcca5b6694bf89295691f1
mausoken <kkind865@gmail.com>
2024-12-11 11:52:51 +0600

    premiere

--- src/Main.java
+++ src/Main.java
@@ -8,42 +8,49 @@ public class Main {
         Author mitski = new Author("Mitski", "Matsubara");
 
         // Creating music objects
-        Music birdsOfAFeather = new Music("Symphony", cleanBandit, pop,
-                "https://www.youtube.com/watch?v=V9PVRfjEBTI", 246, 40);
+        Music skyfall = new Music("Skyfall", cleanBandit, pop,
+                "https://www.youtube.com/watch?v=sZrTJesvJeo", 246, 40);
         Music nightCall = new Music("Night Call", kavinsky, pop,
-                "https://www.youtube.com/watch?v=k2tRMP-V5y4", 256, 55); // Fixed the syntax error here
+                "https://www.youtube.com/watch?v=MT3EzPOE00M", 256, 55);
         Music realHero = new Music("Real Hero", cleanBandit, pop,
-                "https://www.youtube.com/watch?v=fwfjsDsMuz8", 267, 60);
-        Music myLoveMineAllMine = new Music("My Love Mine All Mine", mitski, pop,
-                "https://www.youtube.com/watch?v=jFBJnbTW_z8", 138, 50);
+                "https://www.youtube.com/watch?v=lBQUxEgPqUM", 267, 60);
+        Music phonique = new Music("My Love Mine All Mine", mitski, pop,
+                "https://www.youtube.com/watch?v=81c-alO8TVQ", 138, 50);
         Music neverGoneGiveYouAp = new Music("Never Gone Give You Ap", cleanBandit, pop,
                 "https://www.youtube.com/watch?v=vnYm_kguurE", 212, 40);
 
         // Creating users
         User ryan = new User("Ryan Gosling", "POP", 500);
-        User elzar = new User("Elzar Zholdoshev", "Metal", 120);
+        User adis = new User("Adis Askarbekov", "Metal", 120);
 
         // Users buying music
         ryan.buyMusic(nightCall, 5);
-        elzar.buyMusic(birdsOfAFeather, 3);
+        adis.buyMusic(skyfall, 3);
 
-        // Display balance and playlist
+        // Display balance and playlist for Ryan
+        System.out.println("Ryan's Balance: ");
         ryan.getBalance();
+        System.out.println("Ryan's Playlist: ");
         ryan.getPlaylist();
-        elzar.getBalance();
-        elzar.getPlaylist();
+
+        // Display balance and playlist for Adis
+        System.out.println("Adis's Balance: ");
+        adis.getBalance();
+        System.out.println("Adis's Playlist: ");
+        adis.getPlaylist();
 
         // Users listening to music
         ryan.listenMusic(realHero);
         ryan.listenMusic(nightCall);
-        elzar.listenMusic(myLoveMineAllMine);
+        adis.listenMusic(phonique);
 
-        // Display music stats
-        myLoveMineAllMine.getDownloads();
-        myLoveMineAllMine.getViews();
-        myLoveMineAllMine.getRate();
+        // Display music stats for "My Love Mine All Mine"
+        System.out.println("Music Stats for 'My Love Mine All Mine': ");
+        phonique.getDownloads();
+        phonique.getViews();
+        phonique.getRate();
 
-        // Open video for a specific music
-        neverGoneGiveYouAp.openVideo();
+        // Open video for "Never Gone Give You Ap"
+        skyfall.openVideo();
     }
 }
